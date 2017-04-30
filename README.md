eb64c3bf4e7fc1be5b3ebb845ac92ee046b0bc83
brandon-schabel <brandonschabel1995@gmail.com>
2017-04-30 09:46:50 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# learning-vuex" 
+to run code open any of the section-code-# and do an "npm install"
+then run "npm run dev"
---
+++ section-code-4/src/components/AnotherCounter.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="increment">Increment</button>
+        <button class="btn btn-primary" @click="decrement">Decrement</button>
+    </div>
+</template>
+
+<script>
+    import {mapMutations} from 'vuex'; 
+    export default {
+
+
+        methods: {
+            ...mapMutations([
+                'increment',
+                'decrement'
+            ])
+            /*
+            increment() {
+                //this.$store gives us access to the central store
+                //this.$store.state.counter++;
+                //instead of using code inside here we can do it using mutations from the state
+                this.$store.commit('increment');
+            },
+            decrement() {
+                //this.$store.state.counter--;
+                this.$store.commit('decrement');
+            }
+            */
+        }
+    }
+</script>
